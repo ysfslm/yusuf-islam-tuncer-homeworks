b5f945b6732c21a77683eef31e3f62020979305f
Yusuf İslam Tuncer <y.islam9920@gmail.com>
2023-03-02 00:41:19 +0300

    completed hw-04.

--- src/main/java/edu/sabanciuniv/hw04/Hw04Application.java
+++ src/main/java/edu/sabanciuniv/hw04/Hw04Application.java
@@ -2,6 +2,8 @@ package edu.sabanciuniv.hw04;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class Hw04Application {
